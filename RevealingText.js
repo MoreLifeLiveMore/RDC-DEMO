db4226db1adcea4fde03cb10e3eeb7c9cf5ea207
MoreLifeLiveMore <aadon.miller@gmail.com>
2024-10-08 00:34:00 -0400

    text message/room change animations

---
+++ RevealingText.js
@@ -0,0 +1,29 @@
+class RevealingText {
+  constructor(config) {
+    this.element = config.element;
+    this.text = config.text;
+    this.speed = config.speed || 70;
+
+    this.timeout = null;
+    this.isDone = false;
+  }
+
+  init() {
+    let characters = [];
+    this.text.split("").forEach((character) => {
+      //create each span, add to element in DOM
+      let span = document.createElement("span");
+      span.textContent = character;
+      this.element.appendChild(span);
+
+      // add this span to internal state array
+      characters.push({
+        span,
+        delayAfter: character === " " ? 0 : this.speed,
+      });
+
+    });
+
+    
+  }
+}
--- TextMessage.js
+++ TextMessage.js
@@ -10,10 +10,15 @@ class TextMessage {
     this.element = document.createElement("div");
     this.element.classList.add("TextMessage");
 
-    this.element.innerHTML = (`
-      <p class="TextMessage_p">${this.text}</p>
+    this.element.innerHTML = `
+      <p class="TextMessage_p"></p>
       <button class="TextMessage_button">Next</button>
-    `)
+    `;
+    //init typewriter effect
+    this.revealingText = new RevealingText({
+      element: this.element.querySelector(".TextMessage_p"),
+      text: this.text,
+    });
 
     this.element.querySelector("button").addEventListener("click", () => {
       //Close the text message
@@ -23,8 +28,7 @@ class TextMessage {
     this.actionListener = new KeyPressListener("Enter", () => {
       this.actionListener.unbind();
       this.done();
-    })
-
+    });
   }
 
   done() {
@@ -34,7 +38,7 @@ class TextMessage {
 
   init(container) {
     this.createElement();
-    container.appendChild(this.element)
+    container.appendChild(this.element);
+    this.revealingText.init();
   }
-
 }
